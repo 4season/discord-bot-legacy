cf43ccb69a3749442ac3fe7999a1f8fe82b2f490
FourSeason <junhoheo11@gmail.com>
2021-06-07 23:17:06 +0900

    Add files via upload

--- node-main.js
+++ node-main.js
@@ -1,6 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const request = require('request'),
+    cheerio = require('cheerio'),
+    jschardet = require('jschardet'),
+    iconv = require('iconv-lite');
+
+
 client.on('ready', () => {
     try {
         console.log(`Logged in as ${client.user.tag}!`);
@@ -11,9 +17,66 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if(msg.content === "무한~") {
-        msg.reply("무~야호~!");
+    try {
+        if(msg.content === "무한~") {
+            msg.reply("무~야호~!");
+        }
+        const msgStr = msg.split(" ");
+        if(msgStr[0] === "/메이플공지") {
+            msg.reply(getNotice());
+        }
+    } catch (err) {
+        msg.reply(err);
+        console.error(err);
     }
 });
 
-client.login(process.env.TOKEN);
+
+const getNotice = () => {
+    request( {
+            url: "https://maplestory.nexon.com/News/Notice",
+            method: "GET"
+        },
+        (error, response, body) => {
+            if(error) {
+                console.error(error);
+                return;
+            }
+            else {
+                (response.statusCode === 200)
+            }
+            console.log("Ready");
+
+            //console.log(body);
+
+            const $ = cheerio.load(body);
+            const taglist = $("#container > div > div > div.news_board > ul > li").toArray(); //.news_board
+            const tagArr = [];
+            taglist.forEach((li) => {
+                const TagF = $(li).find("a").first();
+                const path = TagF.attr("href");
+                const url = `https://maplestory.nexon.com${path}`;
+                const title = TagF.text().trim();
+
+                //console.log(url, title);
+
+                const TagL = $(li).find("dd").last();
+                let date = TagL.text().trim();
+
+                if(date === '') {
+                    date = "Null";
+                }
+
+                //console.log(date);
+
+                tagArr.push({
+                    url,
+                    title,
+                    date,
+                });
+            });
+            console.log(tagArr);
+        });
+}
+
+client.login("process.env.TOKE");
