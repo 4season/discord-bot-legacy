53536c5ea899851b4fa12b30d3c685818053de3a
FourSeason <junhoheo11@gmail.com>
2021-06-07 21:46:45 +0900

    Add files via upload

---
+++ node-main.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    try {
+        console.log(`Logged in as ${client.user.tag}!`);
+    }
+    catch (err) {
+        console.error(err);
+    }
+});
+
+client.on('message', msg => {
+    if(msg.content === "무한~") {
+        msg.reply("무~야호~!");
+    }
+});
+
+client.login("process.env.TOKE");
