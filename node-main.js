8d55af55b52222fe52bb95b7b0acca7fe93abdd4
FourSeason <junhoheo11@gmail.com>
2021-06-15 18:48:51 +0900

    Add files via upload

--- node-main.js
+++ node-main.js
@@ -16,6 +16,7 @@ const request = require('request'),
     iconv = require('iconv-lite');
 
 let tagArr = [];
+let count = 0;
 
 
 client.on('ready', () => {
@@ -24,6 +25,7 @@ client.on('ready', () => {
     }
     catch (err) {
         console.error(err);
+        return;
     }
 });
 
@@ -40,91 +42,89 @@ client.on('message', (msg) => {
         const msgStr = msgTnt.split(" ");
         if(msgStr[0] === "/메이플공지") {
             getNotice(msg);
-            console.log(tagArr);
+            //console.log(tagArr);
         }
     } catch (err) {
         msg.reply(err);
         console.error(err);
+        return;
     }
 });
 
 const getNotice = (msg) => {
-    request( {
-            url: "https://maplestory.nexon.com/News/Notice",
-            method: "GET"
-        },
-        (error, response, body) => {
-            if(error) {
-                console.error(error);
-                return;
-            }
-            else {
-                (response.statusCode === 200)
-            }
-            console.log("Ready");
-
-            //console.log(body);
-            const $ = cheerio.load(body);
-            const taglist = $("#container > div > div > div.news_board > ul > li").toArray(); //.news_board
-            taglist.forEach((li) => {
-                const TagF = $(li).find("a").first();
-                const path = TagF.attr("href");
-                const url = `https://maplestory.nexon.com${path}`;
-                const title = TagF.text().trim();
-
-                //console.log(url, title);
-
-                const TagL = $(li).find("dd").last();
-                let date = TagL.text().trim();
-
-                if(date === '') {
-                    date = "Null";
-                }
-                //console.log(date);
-                let count = 0;
-                if(tagArr[0] === '') {
-                    tagArr.push({"url": url, "title": title, "date": date});
-                    msgEmbed0.setColor('9461ee');
-                    msgEmbed0.setTitle('공지사항 결과');
-                    msgEmbed0.setDescription(`최근 공지사항 ${tagArr.length}개 항목을 가져옵니다.\n\u200B`);
-                    emdFor(msg, count);
+    try {
+        request({
+                url: "https://maplestory.nexon.com/News/Notice",
+                method: "GET"
+            },
+            (error, response, body) => {
+                if (error) {
+                    console.error(error);
+                    return;
                 } else {
-                    emdFor(msg, count);
+                    (response.statusCode === 200)
                 }
-            });
+                console.log("Ready");
 
-/*
-                for (let i = 0; i < tagArr.length; i++) {
-                    count++;
-                    if(count === tagArr.length) {
-                        count = 0;
-                        msgEmbed0.spliceFields(0, tagArr.length);
-                        emdFor0(count);
-                    } else {
-                        emdFor0(count);
+                //console.log(body);
+                const $ = cheerio.load(body);
+                const taglist = $("#container > div > div > div.news_board > ul > li").toArray(); //.news_board
+                taglist.forEach((li) => {
+                    const TagF = $(li).find("a").first();
+                    const path = TagF.attr("href");
+                    const url = `https://maplestory.nexon.com${path}`;
+                    const title = TagF.text().trim();
+
+                    //console.log(url, title);
+
+                    const TagL = $(li).find("dd").last();
+                    let date = TagL.text().trim();
+
+                    if (date === '') {
+                        date = "Null";
                     }
-                }
- */
 
-    });
+                    if (tagArr[0] === '') {
+                        tagArr.push({"url": url, "title": title, "date": date});
+                        emdFor(msg);
+                    } else {
+                        emdFor(msg);
+                    }
+
+                });
+            });
+    } catch (err) {
+        console.error(err);
+        return;
+    }
 }
 
-const emdFor = (msg, count) => {
+const emdFor = (msg) => {
+    try {
+
+    msgEmbed0.setColor('9461ee');
+    msgEmbed0.setTitle('공지사항 결과');
+    msgEmbed0.setDescription(`최근 공지사항 ${tagArr.length}개 항목을 가져옵니다.\n\u200B`);
+
     for(let i = 0; i < tagArr.length; i++) {
-        if(count < tagArr.length-1) {
+        if(i < tagArr.length) {
             count++;
             msgEmbed0.addFields({
                 name: `${count}. ${tagArr[i].title} \n 작성일: ${tagArr[i].date}`, value: `${tagArr[i].url}`, inline: false
             });
-        } else if(count === tagArr.length-1) {
+        } else if(count === tagArr.length) {
             msg.channel.send(msgEmbed0);
-            count = 0;
-            msgEmbed0.spliceFields(0, tagArr.length-1);
-            tagArr.splice(0, tagArr.length-1);
             break;
         }
     }
-    msg.channel.send(msgEmbed0);
+        count = 0;
+        msgEmbed0.spliceFields(0, tagArr.length-1);
+        tagArr.splice(0, tagArr.length-1);
+
+    } catch (err) {
+        console.error(err);
+        return;
+    }
 }
 
 
